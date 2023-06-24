037ed9e6539feed5a1d5931f7ef673a33081ec2e
luizsant <luizsant@gmail.com>
2023-06-24 15:23:38 -0300

    correções

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,31 +1,5 @@
 AOS.init();
 
-// const dataDoEvento = new Date('Jul 12, 2023, 19:00:00');
-// const timeStampDoEvento = dataDoEvento.getTime();
-
-// const contaAsHoras = setInterval(function() {
-//     const agora = new Date();
-//     const timeStampAtual = agora.getTime();
-
-//     const distanciaAteoEvento = timeStampDoEvento - timeStampAtual;
-
-//     const diaEmMs = 1000 * 60 * 60 * 24;
-//     const horaEmMs = 1000 * 60 * 60;
-//     const minutosEmMs = 1000 * 60;
-//     const segundosEmMs = 1000;
-
-//     const diasAteOEvento = Math.floor(distanciaAteoEvento / diaEmMs);
-//     const horasAteOEvento = Math.floor((distanciaAteoEvento % diaEmMs) / horaEmMs);
-//     const minutosAteOEvento = Math.floor((distanciaAteoEvento % horaEmMs) / minutosEmMs);
-//     const segundosAteOEvento = Math.floor((distanciaAteoEvento % minutosEmMs) / segundosEmMs);
-
-//     console.log(diasAteOEvento);
-//     console.log(horasAteOEvento);
-//     console.log(minutosAteOEvento);
-//     console.log(segundosAteOEvento);
-
-//     document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`
-// }, 1000);
 
 const dataDoEvento = new Date('Jul 12, 2023, 19:00:00');
 const timeStampDoEvento = dataDoEvento.getTime();
